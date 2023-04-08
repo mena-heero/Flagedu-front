e8701da87b069d820234d3f23d33af81f31ad9e5
Amir Mubarak <mubarak117136@gmail.com>
2023-04-09 03:28:44 +0600

    update

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueFbCustomerChat from "vue-fb-customer-chat";
 
 Vue.use(VueFbCustomerChat, {
-  page_id: "1437355333159545", //  change 'null' to your Facebook Page ID,
+  page_id: "104198602648752", //  change 'null' to your Facebook Page ID,
   theme_color: "#333333", // theme color in HEX
   locale: "en_US", // default 'en_US'
 });
