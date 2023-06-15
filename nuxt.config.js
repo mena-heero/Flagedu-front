2ae29f71f888f6f968c890f988f9da327b19d84a
Amir Mubarak <mubarak117136@gmail.com>
2023-06-15 12:34:41 +0600

    update icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
       { rel: "stylesheet", href: "/css/bootstrap.min.css" },
       {
         rel: "stylesheet",
-        href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css",
+        href: "/css/bootstrap-icons.css",
       },
       {
         rel: "apple-touch-icon",
